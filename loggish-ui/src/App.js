a382b9e36e888b3a82beabfd62593dc8e42f2cb6
Ben Friedland <ben174@gmail.com>
2017-11-29 18:37:01 -0800

    Remove more boilerplate

--- loggish-ui/src/App.js
+++ loggish-ui/src/App.js
@@ -79,18 +79,6 @@ class App extends Component {
             {this.state.logset.map(log =>
               <Endpoint key={log.endpoint} {...log} />
             )}
-            <Divider inverted section />
-            <Image
-              centered
-              size='mini'
-              src='/logo.png'
-            />
-            <List horizontal inverted divided link>
-              <List.Item as='a' href='#'>Site Map</List.Item>
-              <List.Item as='a' href='#'>Contact Us</List.Item>
-              <List.Item as='a' href='#'>Terms and Conditions</List.Item>
-              <List.Item as='a' href='#'>Privacy Policy</List.Item>
-            </List>
           </Container>
         </Segment>
       </div>
