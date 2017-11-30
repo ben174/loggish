89d48eafefc80bafc5a1cc1bf9b9b213f62c3f28
Ben Friedland <ben174@gmail.com>
2017-11-29 18:31:10 -0800

    Clean up branding

--- loggish-ui/src/App.js
+++ loggish-ui/src/App.js
@@ -60,48 +60,20 @@ class App extends Component {
     return ret
   }
 
-
   render() {
     return (
-
-
-
       <div>
         <Menu fixed='top' inverted>
           <Container>
             <Menu.Item as='a' header>
-              <Image
-                size='mini'
-                src='/logo.png'
-                style={{ marginRight: '1.5em' }}
-              />
-              Project Name
+              Loggish
             </Menu.Item>
             <Menu.Item as='a'>Home</Menu.Item>
-
-            <Dropdown item simple text='Dropdown'>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Header>Header Item</Dropdown.Header>
-                <Dropdown.Item>
-                  <i className='dropdown icon' />
-                  <span className='text'>Submenu</span>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
           </Container>
         </Menu>
-
         <Segment
           vertical
-          style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
+          style={{ margin: '0em 0em 0em', padding: '5em 0em' }}
         >
           <Container textAlign='center'>
             {this.state.logset.map(log =>
